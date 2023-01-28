7b99773e89bf00bcc77761721d3ab03e7445d94c
Brendan Bushbaker <bushbak2@msu.edu>
2023-01-28 13:15:18 -0500

    Getting the first thing on screen.

--- Lib/Game.py
+++ Lib/Game.py
@@ -1,4 +1,5 @@
 import pygame
+import sys
 
 class Game:
     def __init__(self):
@@ -7,6 +8,17 @@ class Game:
         self.start_game()
 
     def start_game(self):
+        """
+        Start the game
+        :return:
+        """
         pygame.init()
+        black = 0, 0, 0
         self.screen = pygame.display.set_mode(self.dimensions)
-
+        while True:
+            self.screen.fill(black)
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    sys.exit()
+                    break
+            pygame.display.flip()
