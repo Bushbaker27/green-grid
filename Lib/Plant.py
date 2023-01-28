db4905a2d29e16317e8de6191eb330ccb2cd3bde
alexggrundy <grundyal@msu.edu>
2023-01-28 15:50:06 -0500

    created grid and changed plant.py

--- Lib/Plant.py
+++ Lib/Plant.py
@@ -1,9 +1,24 @@
 import pygame
-
+import json
 class Plant(pygame.sprite.Sprite):
-    def __init__(self, x, y, image):
+    def __init__(self, x, y, image, row, col, name):
         pygame.sprite.Sprite.__init__(self)
         self.image = image
         self.rect = self.image.get_rect()
         self.rect.x = x
         self.rect.y = y
+        self.row = row
+        self.col = col
+        self.name = name
+
+    def relations(self):
+        #opens JSON file
+        f = open('CropDB.json')
+
+        #returns JSON object as dict
+        data = json.load(f)
+
+        for i in data['crops']:
+            print(i)
+
+        f.close()
