7bfae332cb3e94eac1a3f2aab0d09b3bc4164483
cirenese <cirenese@msu.edu>
2023-01-28 21:36:18 -0500

    Working text responding

--- Lib/SendSMS.py
+++ Lib/SendSMS.py
@@ -8,7 +8,7 @@ from pyngrok import ngrok, conf, installer
 pyngrok_config = conf.get_default()
 
 if not os.path.exists(pyngrok_config.ngrok_path):
-    myssl = ssl.create_default_context();
+    myssl = ssl.create_default_context()
     myssl.check_hostname=False
     myssl.verify_mode=ssl.CERT_NONE
     installer.install_ngrok(pyngrok_config.ngrok_path, context=myssl)
