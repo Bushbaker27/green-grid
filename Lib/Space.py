86d75718f8c71f4dfb01875957e47f15144810d2
Brendan Bushbaker <bushbak2@msu.edu>
2023-01-28 19:51:56 -0500

    Grid draw and helpers

--- Lib/Space.py
+++ Lib/Space.py
@@ -7,6 +7,23 @@ class Space:
     """
     def __init__(self, location):
         self.item = None
-        self.color = pg.Color('brown')
+        self.color = pg.Color('green')
         self.location = location
+
+    def draw(self, screen):
+        """
+        Draws the space to the screen.
+        :param screen:
+        :return:
+        """
+        # The rectangle of the space.
+        space_rect = pg.Rect(495 + self.location[1] * 10, 10 + self.location[0] * 10, 10, 10)
+        pg.draw.rect(screen, self.color, space_rect)
+
+    def get_item(self):
+        """
+        Gets the item in the space.
+        :return:
+        """
+        return self.item
 
